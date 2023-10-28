41234816ce6e8dec24e34c6882d62ab684693193
anna <845632255@qq.com>
2023-10-28 14:36:22 +0800

    MergeTree

--- clickHouse/MergeTree/AggregatingMergeTree.sql
+++ clickHouse/MergeTree/AggregatingMergeTree.sql
@@ -1,26 +1,30 @@
-CREATE TABLE agg_table_basic(
-    id String,
-    city String,
-    code String,
+CREATE TABLE agg_table_basic
+(
+    id    String,
+    city  String,
+    code  String,
     value UInt32
-)ENGINE = MergeTree()
-PARTITION BY city
-ORDER BY (id,city);
+) ENGINE = MergeTree()
+      PARTITION BY city
+      ORDER BY (id, city);
 
 CREATE MATERIALIZED VIEW agg_view
-ENGINE = AggregatingMergeTree()
-PARTITION BY city
-ORDER BY (id,city)
-AS SELECT
-id,
-city,
-uniqState(code) AS code,
-sumState(value) AS value
+            ENGINE = AggregatingMergeTree()
+                PARTITION BY city
+                ORDER BY (id, city)
+AS
+SELECT id,
+       city,
+       uniqState(code) AS code,
+       sumState(value) AS value
 FROM agg_table_basic
 GROUP BY id, city;
 
-INSERT INTO TABLE agg_table_basic VALUES
-('A000','wuhan','code1',100),
-('A000','wuhan','code2',200),
-('A000','zhuhai', 'code1',200);
-SELECT id, sumMerge(value), uniqMerge(code) FROM agg_view GROUP BY id,city
+INSERT INTO TABLE agg_table_basic
+VALUES ('A000', 'wuhan', 'code1', 100),
+       ('A000', 'wuhan', 'code2', 200),
+       ('A000', 'zhuhai', 'code1', 200);
+
+SELECT id, sumMerge(value), uniqMerge(code)
+FROM agg_view
+GROUP BY id, city;
